aca6b7a6ed47a187117e4a9b0dd0baa43bc5d9ea
gourab98 <gourabsahag@gmail.com>
2021-09-14 21:53:04 +0600

    Image resizing added in test_app

--- test_app/lib/main.dart
+++ test_app/lib/main.dart
@@ -1,27 +1,133 @@
 import 'package:flutter/material.dart';
-//import 'dart:async' show Future;
-//import 'package:flutter/services.dart' show rootBundle;
+import 'dart:math' as math;
+
+//import 'package:flutter/src/painting/image_resolution.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: CollapsingList(),
+      ),
+    );
+  }
+}
+
+class _SliverAppBarDelegate extends SliverPersistentHeaderDelegate {
+  _SliverAppBarDelegate({
+    required this.minHeight,
+    required this.maxHeight,
+    required this.child,
+  });
+  final double minHeight;
+  final double maxHeight;
+  final Widget child;
+  @override
+  double get minExtent => minHeight;
+  @override
+  double get maxExtent => math.max(maxHeight, minHeight);
+  @override
+  Widget build(
+      BuildContext context, double shrinkOffset, bool overlapsContent) {
+    return new SizedBox.expand(child: child);
+  }
+
+  @override
+  bool shouldRebuild(_SliverAppBarDelegate oldDelegate) {
+    return maxHeight != oldDelegate.maxHeight ||
+        minHeight != oldDelegate.minHeight ||
+        child != oldDelegate.child;
+  }
+}
+
+class CollapsingList extends StatelessWidget {
+  SliverPersistentHeader makeHeader(
+    Image img,
+    String headerText,
+    double minHeight,
+    double maxHeight,
+    Color color,
+  ) {
+    return SliverPersistentHeader(
+      pinned: true,
+      delegate: _SliverAppBarDelegate(
+        minHeight: minHeight,
+        maxHeight: maxHeight,
+        child: Container(
+          color: color,
+          child: Center(
+            // child: Text(
+            //   headerText,
+            //   style: TextStyle(color: Colors.white),
+            // ),
+            child: Image(image: AssetImage('assets/sam-photo.jpg')),
+          ),
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return CustomScrollView(
+      slivers: <Widget>[
+        // Container(
+        //   child: Image(
+        //     image: AssetImage('assets/sam-photo.jpg'),
+        //     fit: BoxFit.fill,
+        //   ),
+        // ),
+
+//        makeHeader('Pinned', 100, 100, Colors.blue),
+//        makeHeader('Floating', 0, 250, Colors.green),
+        makeHeader(Image(image: AssetImage('assets/sam-photo.jpg')),
+            'Please Run', 100, 400, Colors.green),
+
+        SliverList(
+            delegate: SliverChildListDelegate([
+          // Container(
+          //   child: Image(
+          //     image: AssetImage('assets/sam-photo.jpg'),
+          //     fit: BoxFit.fill,
+          //   ),
+          // ),
+          Container(
+              height: 2000,
+              color: Colors.white,
+              child: Center(
+                  child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                    Text('TOP'),
+                    Text('MIDDLE'),
+                    Text('BOTTOM'),
+                  ]))),
+        ]))
+      ],
+    );
+  }
+}
+
+
+
+
+/*
+
+import 'package:flutter/material.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.green,
       ),
       home: MyHomePage(title: 'Flutter Page'),
@@ -31,15 +137,6 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -53,15 +150,12 @@ class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
   String _title = "I am nobody";
 
+  var backgroundColor;
+
   get mainAxisAlignment => null;
 
   void _incrementCounter() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
       _counter++;
       _title = "I am Supreme. $_counter";
     });
@@ -69,60 +163,76 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-//        title: Text(widget.title),
         title: Text('$_title'),
       ),
-      body: Container(
-        child:
-            Row(mainAxisAlignment: MainAxisAlignment.center, children: <Widget>[
+      body: CustomScrollView(
+        slivers: [
+          SliverAppBar(
+            pinned: true,
+            title: Text('Pinned App Bar'),
+          ),
+          SliverPersistentHeader(
+            floating: false,
+            delegate: FloatingHeader(),
+          ),
           Container(
-            margin: EdgeInsets.all(12.0),
-            padding: EdgeInsets.all(8.0),
-            decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(8), color: Colors.green),
-            child: Text(
-              "React.js",
-              style: TextStyle(color: Colors.yellowAccent, fontSize: 25),
+            child: AspectRatio(
+              aspectRatio: 1,
+              child: Image(
+                height: 300,
+                width: 300,
+                image: AssetImage('assets/sam-photo.jpg'),
+                fit: BoxFit.fill,
+              ),
             ),
           ),
           Container(
-            margin: EdgeInsets.all(15.0),
-            padding: EdgeInsets.all(8.0),
-            decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(8), color: Colors.green),
-            child: Text(
-              "Flutter",
-              style: TextStyle(color: Colors.yellowAccent, fontSize: 25),
+            height: 2000,
+            color: Colors.white,
+            child: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text('TOP'),
+                  Text('MIDDLE'),
+                  Text('BOTTOM'),
+                ],
+              ),
             ),
           ),
-          Container(
-            margin: EdgeInsets.all(12.0),
-            padding: EdgeInsets.all(8.0),
-            decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(8), color: Colors.green),
-            child: Text(
-              "MySQL",
-              style: TextStyle(color: Colors.yellowAccent, fontSize: 25),
-            ),
-          )
-        ]),
+        ],
       ),
-
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
         tooltip: 'Increment',
         child: Icon(Icons.add_sharp),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      ),
     );
   }
 }
+
+class FloatingHeader extends SliverPersistentHeaderDelegate {
+  @override
+  Widget build(
+      BuildContext context, double shrinkOffset, bool overlapsContent) {
+    return Container(
+      color: Colors.red,
+      child: Center(
+          child:
+              Text('Floating Header', style: TextStyle(color: Colors.white))),
+    );
+  }
+
+  @override
+  bool shouldRebuild(SliverPersistentHeaderDelegate _) => true;
+
+  @override
+  double get maxExtent => 250.0;
+
+  @override
+  double get minExtent => 80.0;
+}
+
+*/
