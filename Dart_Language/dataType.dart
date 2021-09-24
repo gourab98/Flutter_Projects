7b6f8bd0512d7c9872427359aa9f7ce492493d57
gourab98 <gourabsahag@gmail.com>
2021-09-24 09:09:33 +0600

    adding dart_language(DataType)

---
+++ Dart_Language/dataType.dart
@@ -0,0 +1,11 @@
+void main() {
+  int Date = 23;
+  var Month = 9; // It is inferred as integer automatically
+  double Year = 2011;
+
+  String FirstName = "Ranit";
+  var LastName = "Saha";
+
+  print(
+      'The person name is $FirstName $LastName and his birthday is $Date/$Month/$Year');
+}
