727c705ee72b2b2b114b55738f5630d375aecaac
gourab98 <gourabsahag@gmail.com>
2021-09-24 10:36:19 +0600

    adding dart_language(DataType2)

---
+++ Dart_Language/dataType2.dart
@@ -0,0 +1,22 @@
+void main() {
+  final cityName = 'Dhaka';
+//  cityName = 'Gopalganj';
+
+  var continental = 'America';
+
+  continental = 'Asia';
+
+  final String countryName = 'Bangladesh';
+
+  const PI = 3.14159;
+  const double gravity = 9.8;
+
+  Circle c1 = Circle();
+  print(c1.color);
+  print(Circle.PIE);
+}
+
+class Circle {
+  final color = 'Black';
+  static const PIE = 3.14;
+}
