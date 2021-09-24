2918095e7c59ec621f849d0794872656bd7ba540
gourab98 <gourabsahag@gmail.com>
2021-09-24 09:33:09 +0600

    adding dart_language(DataType)

--- Dart_Language/dataType.dart
+++ Dart_Language/dataType.dart
@@ -8,4 +8,29 @@ void main() {
 
   print(
       'The person name is $FirstName $LastName and his birthday is $Date/$Month/$Year');
+
+  Value();
+  StringManupulation();
+}
+
+void Value() {
+  int HexValue = 0xAAAEEEEEEE;
+  double Exponents = 1.45e7;
+
+  print(HexValue);
+  print(Exponents);
+}
+
+void StringManupulation() {
+  String s1 = 'Single ';
+  String s2 = "And Double. ";
+  String s3 = 'So it\'s normal to get confused. ';
+  String s4 = "You can use it's in double quote, ";
+  String s5 = 'This is a string. ' 'And it is an another string. ';
+  String s6 = 'Amit.';
+  String s7 = "And obviously I am " + s6;
+  String s8 = "Please Listen, $s6";
+  String s9 = s1 + s2 + s3 + s4 + s5 + s7 + s8;
+
+  print(s9);
 }
