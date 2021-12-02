87166c60579caec6db7cd3ab34100c165c324954
gourab98 <gourabsahag@gmail.com>
2021-12-02 12:47:43 +0600

    added ludoboard's 15 X 15 cointainer box.

--- ludo/lib/ludo_board.dart
+++ ludo/lib/ludo_board.dart
@@ -20,6 +20,36 @@ class _LudoBoardState extends State<LudoBoard> {
       appBar: AppBar(
         title: Text("${widget.colorName}  ${widget.playerName}"),
       ),
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.only(top: 10.0),
+          child: AspectRatio(
+            aspectRatio: 1,
+            child: Stack(
+//              alignment: Alignment.topCenter,
+              children: <Widget>[
+                GridView.count(
+                  crossAxisCount: 15,
+                  childAspectRatio: 1,
+                  children: List<Widget>.generate(
+                    225,
+                    (index) {
+                      return GridTile(
+                        child: Container(
+                          decoration: BoxDecoration(
+                            border: Border.all(color: Colors.black),
+                          ),
+//                          child: Text("$index"),
+                        ),
+                      );
+                    },
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
