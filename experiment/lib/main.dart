0c9bbd463f9b2212641cdc8672bd300db4bfc48d
gourab98 <gourabsahag@gmail.com>
2021-12-08 11:24:50 +0600

    Experiments on Stack and gesture dectector

--- experiment/lib/main.dart
+++ experiment/lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -37,46 +39,78 @@ class _MyHomePageState extends State<MyHomePage> {
       body: Center(
         child: Stack(
           children: <Widget>[
-            Center(
-              child: GestureDetector(
-                // behavior: HitTestBehavior.translucent,
-                onTap: () {
-                  print("1st Time: 1st Time: 1st Time- 1111111111111111");
-                },
-                child: Container(
-                  child: Center(
-                    child: Text(
-                      "1st",
-                      style: TextStyle(fontSize: 30),
+            GestureDetector(
+              behavior: HitTestBehavior.translucent,
+              onTap: () {
+                print("I am being pressed");
+              },
+              child: GridView.count(
+                crossAxisCount: 15,
+                childAspectRatio: 1,
+                children: List.generate(225, (index) {
+                  return Stack(
+                    children: [
+                      GridTile(
+                        child: Container(
+                          decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(5),
+                            border: Border.all(color: Colors.black),
+                          ),
+                        ),
+                      ),
+                    ],
+                  );
+                }),
+              ),
+            ),
+            Positioned(
+              height: 1000,
+              width: 200,
+              child: Center(
+                child: GestureDetector(
+                  // behavior: HitTestBehavior.translucent,
+                  onTap: () {
+                    print("1st Time: 1st Time: 1st Time- 1111111111111111");
+                  },
+                  child: Container(
+                    child: Center(
+                      child: Text(
+                        "1st",
+                        style: TextStyle(fontSize: 30),
+                      ),
                     ),
+                    height: 100,
+                    width: 200,
+                    color: Colors.amber,
                   ),
-                  height: 200,
-                  width: 200,
-                  color: Colors.amber,
+                ),
+              ),
+            ),
+            Positioned(
+              height: 800,
+              width: 200,
+              child: Container(
+                child: GestureDetector(
+                  onTap: () {
+                    print(
+                        "2nd Time: 2nd Time: 2nd Time - 22222222222222222222");
+                  },
+                  child: Container(
+                    child: Center(
+                      child: Text(
+                        "2nd",
+                        style: TextStyle(fontSize: 30),
+                      ),
+                    ),
+                    height: 100,
+                    width: 100,
+                    color: Colors.cyan,
+                  ),
                 ),
               ),
             ),
             Container(
-              alignment: Alignment.topCenter,
-              child: GestureDetector(
-                onTap: () {
-                  print("2nd Time: 2nd Time: 2nd Time - 22222222222222222222");
-                },
-                child: Container(
-                  child: Center(
-                    child: Text(
-                      "2nd",
-                      style: TextStyle(fontSize: 30),
-                    ),
-                  ),
-                  height: 200,
-                  width: 200,
-                  color: Colors.cyan,
-                ),
-              ),
-            ),
-            Container(
-              alignment: Alignment.bottomCenter,
               child: GestureDetector(
                 onTap: () {
                   print("3nd Time: 3nd Time: 3nd Time - 3333333333333333");
@@ -88,8 +122,8 @@ class _MyHomePageState extends State<MyHomePage> {
                       style: TextStyle(fontSize: 30),
                     ),
                   ),
-                  height: 200,
-                  width: 200,
+                  height: 100,
+                  width: 100,
                   color: Colors.green,
                 ),
               ),
