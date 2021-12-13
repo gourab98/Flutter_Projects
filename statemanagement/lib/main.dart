d2248e9ec2ff048e2dabe9885bbfd77e4a87ee61
gourab98 <gourabsahag@gmail.com>
2021-12-13 17:23:36 +0600

    Adding StateManagement with provider

---
+++ statemanagement/lib/main.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:statemanagement/counter.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider.value(
+          value: Counter(),
+        ),
+      ],
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: const MyHomePage(title: 'State Management System'),
+      ),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  final String title;
+  // ignore: use_key_in_widget_constructors
+  const MyHomePage({required this.title});
+
+  void _incrementCounter(BuildContext context) {
+    Provider.of<Counter>(context, listen: false).incrementCounter();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    var counter = Provider.of<Counter>(context).getCounter;
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$counter',
+              style: Theme.of(context).textTheme.headline4,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          _incrementCounter(context);
+        },
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
